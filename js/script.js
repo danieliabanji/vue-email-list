95edcb65ed845e78f53e5c3828b3cc3a3c1b2691
daniel iabanji <daniel.iabanji@yahoo.com>
2022-11-08 14:53:32 +0100

    genera 10 email in pagina funzionante

--- js/script.js
+++ js/script.js
@@ -2,18 +2,28 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
-      
+            mails: [],
 
         }
     },
-    methods() {
-        
+    methods: {
+        callMail() {
+            const mailsList = [];
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                    console.log(res.data.response);
+                    mailsList.push(res.data.response);
+                    if(i === 9) this.mails = [...mailsList]
+                })
+
+            }
+        },
     },
     computed() {
 
     },
     created() {
-       
+       this.callMail();
 
     },
 
